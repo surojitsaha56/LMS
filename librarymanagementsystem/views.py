7d7d9e51e88e3214f16664456603d0692b291655
Surojit Saha <sahasurojit06@gmail.com>
2021-03-27 12:35:11 +0530

    Made the home page and navbar

--- librarymanagementsystem/views.py
+++ librarymanagementsystem/views.py
@@ -3,9 +3,3 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
-
-def login(request):
-	return render(request, 'login.html')
-
-def menu(request):
-	return render(request, 'menu.html')
