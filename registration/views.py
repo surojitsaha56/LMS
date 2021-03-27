b3e5e41fd9ef6d39b81adaf2fb56cc9728d83123
Surojit Saha <sahasurojit06@gmail.com>
2021-03-27 13:02:11 +0530

    added signup page and working

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user=User.objects.get(username=request.POST['username'])
+                return render(request, 'registration/signup.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
+                user=User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'registration/signup.html', {'error':'Password must match'})
+    else:
+        pass
+    return render(request, 'registration/signup.html')
