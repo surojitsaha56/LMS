eaae7a2ca294385ae99e9ed32be4e3e59f3fe215
Surojit Saha <sahasurojit06@gmail.com>
2021-04-07 21:50:58 +0530

    Added 2 models

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import AddStudent, AddBook
+
+admin.site.register(AddStudent)
+admin.site.register(AddBook)
--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AddStudent(models.Model):
+    BRANCH_CHOICES = (
+    ('cse','CSE'),
+    ('it', 'IT'),
+    ('extc','EXTC'),
+    ('chemical','CHEMICAL'),
+    ('mechanical','MECHANICAL'),
+)
+    YEAR_CHOICES=(
+        ('first', 'FE'),
+        ('second', 'SE'),
+        ('third', 'TE'),
+        ('fourth', 'BE'),
+    )
+    sid=models.IntegerField()
+    sname=models.CharField(max_length=50)
+    dob=models.DateField()
+    branch=models.CharField(max_length=15, choices=BRANCH_CHOICES, default='cse')
+    year=models.CharField(max_length=10, choices=YEAR_CHOICES, default='first')
+
+    def __str__(self):
+        return self.sname
+
+class AddBook(models.Model):
+    bid=models.IntegerField()
+    bname=models.CharField(max_length=50)
+    isbn=models.CharField(max_length=20)
+    author=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.bname
