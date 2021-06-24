21b045d277a1da935b8c6aad3a81488eb9d55a7d
Surojit Saha <sahasurojit06@gmail.com>
2021-06-24 18:07:54 +0530

    Heroku error

--- librarymanagementsystem/settings.py
+++ librarymanagementsystem/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'librarymanagementsystem/static/')]
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'librarymanagementsystem/static/')
 
