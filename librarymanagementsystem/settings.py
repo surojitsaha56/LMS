6dfc013ab3531eadefd5b40f66f8a7917e17f063
Surojit Saha <sahasurojit06@gmail.com>
2021-06-25 11:28:42 +0530

    Revert "Heroku Deploy"
    This reverts commit cc1b2ff02521c41228266b1b2205b41415ffe7e9.

--- librarymanagementsystem/settings.py
+++ librarymanagementsystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://librarymanagementsystem-lms.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
